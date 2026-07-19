901e65f2839f2d90b2eb360dfb87321e8a57099d
Eritosin Salami <salamieritosinlearn@gmail.com>
2026-07-18 20:12:10 -0700

    Add files via upload

--- warehouse/script/gold/ddl_gold.sql
+++ warehouse/script/gold/ddl_gold.sql
@@ -1,19 +1,4 @@
-/*
-===============================================================================
-DDL Script: Create Gold Views
-===============================================================================
-Script Purpose:
-    This script creates views for the Gold layer in the data warehouse. 
-    The Gold layer represents the final dimension and fact tables (Star Schema)
-
-    Each view performs transformations and combines data from the Silver layer 
-    to produce a clean, enriched, and business-ready dataset.
-
-Usage:
-    - These views can be queried directly for analytics and reporting.
-===============================================================================
-*/
-
+use [my_base]
 
 -- =============================================================================
 -- Create Dimension: gold.dim_products
@@ -55,9 +40,10 @@ SELECT
 	s.Order_Date AS order_date,
 	s.Ship_Date AS ship_date,
 	s.Ship_Mode AS ship_mode,
+	s.City AS city,
+	s.Region AS region,
+	s.State_Province AS state_name,
 	pr.product_key,
-	s.Division AS division,
-	s.Product_Name AS product_name,
 	s.Sales AS sales,
 	s.Units AS units,
 	s.Gross_Profit AS gross_profit,
@@ -78,7 +64,9 @@ IF OBJECT_ID ('gold.dim_location', 'V') IS NOT NULL
 DROP VIEW gold.dim_location;
 GO
 CREATE VIEW gold.dim_location AS
-SELECT  
+SELECT * FROM (
+SELECT 
+	ROW_NUMBER () OVER (PARTITION BY s.Customer_ID ORDER BY u.zip) AS rn,
 	u.zip AS zip,
 	s.Customer_ID AS customer_id,
 	u.lat AS latitude,
@@ -99,7 +87,8 @@ FROM silver.uszips u
 LEFT JOIN silver.sales s
 ON u.zip = s.Postal_Code
 WHERE s.Customer_ID IS NOT NULL
-AND u.zip IS NOT NULL;
+AND u.zip IS NOT NULL)t
+WHERE rn = 1;
 GO
 
 
